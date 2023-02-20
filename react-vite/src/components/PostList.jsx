b3b8661a1d93072448b7d8504cc531220789589f
SebLask12 <sebastian.laskowski15@gmail.com>
2023-02-20 19:32:21 +0100

    Finished

--- react-vite/src/components/PostList.jsx
+++ react-vite/src/components/PostList.jsx
@@ -6,29 +6,14 @@ import classes from "./PostList.module.css";
 function PostsList() {
   const posts = useLoaderData();
 
-  function addPostHandler(postData) {
-    console.log(postData);
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
-
-  let modalContent;
-
-  
-
   return (
     <>
       <ul className={classes.posts}>
         {posts.length > 0 &&
           posts.map((post) => (
             <Post
-              key={post.author}
+              key={post.id}
+              id={post.id}
               author={post.author}
               body={post.body}
             />
--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Posts, {loader as postLoader} from "./routes/Posts";
-import NewPost from "./routes/NewPost";
+import Posts, { loader as postLoader } from "./routes/Posts";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 import RootLayout from "./routes/RootLayout";
 import "./index.css";
 
@@ -16,7 +17,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <Posts />,
         loader: postLoader,
-        children: [{ path: "/create-post", element: <NewPost /> }],
+        children: [
+          { path: "/create-post", element: <NewPost />, action: newPostAction },
+          { path: "/:id", element: <PostDetails />, loader: postDetailsLoader },
+        ],
       },
     ],
   },
